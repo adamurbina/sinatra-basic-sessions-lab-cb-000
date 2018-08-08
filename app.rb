532d42c6b2fa361fe68d7984a96a42b8ce10605d
Adam Urbina <adamurbina@gmail.com>
2018-08-08 14:45:28 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -13,6 +13,7 @@ class App < Sinatra::Base
 
     post "/checkout" do
         session[:item] = params[:item]
-        @session = session 
+        @session = session
+        binding.pry 
     end
 end
